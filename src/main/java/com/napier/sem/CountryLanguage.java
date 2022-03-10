58880105e3f8b9a2d84f9efe0d7bc457fdbd32db
Joseph <joegonnella@hotmail.com>
2022-03-10 18:29:13 +0000

    Added CountryLanguage class

---
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -0,0 +1,24 @@
+package com.napier.sem;
+
+public class CountryLanguage {
+
+    /**
+     * CountryLanguage country code
+     */
+    public String country_code;
+
+    /**
+     * CountryLanguage language
+     */
+    public String language;
+
+    /**
+     * CountryLanguage Is it official? (True of False)
+     */
+    public String is_official;
+
+    /**
+     * CountryLanguage percentage of country who speak it
+     */
+    public double percentage;
+}
