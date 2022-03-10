26fe40021815db61fe0f56ca97447efc11a3bfab
Joseph <joegonnella@hotmail.com>
2022-03-10 18:15:42 +0000

    Added Country class

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -1,4 +1,79 @@
 package com.napier.sem;
 
 public class Country {
+
+    /**
+     * Country code
+     */
+    public String code;
+
+    /**
+     * Country name
+     */
+    public String name;
+
+    /**
+     * Country Continent
+     */
+    public String continent;
+
+    /**
+     * Country Region
+     */
+    public String region;
+
+    /**
+     * Country Surface Area
+     */
+    public double surface_area;
+
+    /**
+     * Country Independence Year
+     */
+    public int indep_year;
+
+    /**
+     * Country Population
+     */
+    public int population;
+
+    /**
+     * Country Life Expectancy
+     */
+    public float life_expectancy;
+
+    /**
+     * Country Gross National Product
+     */
+    public float gnp;
+
+    /**
+     * Country Old GNP
+     */
+    public float gnp_old;
+
+    /**
+     * Country Local Name
+     */
+    public String local_name;
+
+    /**
+     * Country Government Form
+     */
+    public String gov_form;
+
+    /**
+     * Country Head of State
+     */
+    public String state_head;
+
+    /**
+     * Country Capital ID
+     */
+    public int capital_id;
+
+    /**
+     * Country 2 letter code
+     */
+    public String code2;
 }
