c2c5235bd4c1760937d818bacbdcf447a2520dcd
Joseph <joegonnella@hotmail.com>
2022-03-10 17:54:27 +0000

    Added City class

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,32 @@
+package com.napier.sem;
+
+/**
+ * Represent a city
+ */
+public class City {
+
+    /**
+     * City ID
+     */
+    public int id;
+
+    /**
+     * City name
+     */
+    public String name;
+
+    /**
+     * City's country code
+     */
+    public String country_code;
+
+    /**
+     * City's district
+     */
+    public String district;
+
+    /**
+     * City's population
+     */
+    public int population;
+}
