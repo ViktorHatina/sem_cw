ef98cbebd5dbf208d0931d6434f5fba29a3fe7f5
ViktorHatina <hatina.viktor@gmail.com>
2022-04-10 21:40:59 +0200

    Added getAllCountriesContPopDesc function which shows highest population countries sorted by continent.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,22 +4,21 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class App
-{
+public class App {
     public static void main(String[] args) {
         // Create new Application and connect to database
         App a = new App();
 
-        if(args.length < 1){
+        if (args.length < 1) {
             a.connect("localhost:33060", 30000);
-        }else{
+        } else {
             a.connect(args[0], Integer.parseInt(args[1]));
         }
         // Print Lang Reports
         //a.printReport(getLanguageReports());
 
         //Print countries by population largest to smallest
-        ArrayList<Country> countries = a.getAllCountriesByPopDesc();
+        ArrayList<Country> countries = a.getAllCountriesContPopDesc("Europe");
         a.printAllCountries(countries);
 
         // Disconnect from database
@@ -56,7 +55,7 @@ public class App
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " +                                  Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
@@ -67,28 +66,22 @@ public class App
     /**
      * Disconnect from the MySQL database.
      */
-    public static void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public static void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
+
     /**
      * Get the World's Population
      */
-    public static long getWorldPopulation()
-    {
-        try
-        {
+    public static long getWorldPopulation() {
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
@@ -97,15 +90,11 @@ public class App
                             + "FROM country";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-            if (rset.next())
-            {
+            if (rset.next()) {
                 return rset.getLong(1);
-            }
-            else
+            } else
                 return 0;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get world population");
             return 0;
@@ -115,15 +104,13 @@ public class App
     /**
      * Output Reports
      */
-    public static void printReport(ArrayList<String> s)
-    {
-        if (s == null)
-        {
+    public static void printReport(ArrayList<String> s) {
+        if (s == null) {
             System.out.println("No Report to Print");
         } else {
-             for (int i=0; i< s.size(); i++) {
+            for (int i = 0; i < s.size(); i++) {
                 System.out.println(s.get(i));
-             }
+            }
         }
     }
 
@@ -132,8 +119,7 @@ public class App
      */
     public static ArrayList<String> getLanguageReports() {
         ArrayList<String> output = new ArrayList<String>();
-        try
-        {
+        try {
             long wpop = getWorldPopulation();
             // Create an SQL statement
             Statement stmt = con.createStatement();
@@ -148,17 +134,14 @@ public class App
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
 
-            while (rset.next())
-            {
+            while (rset.next()) {
                 String n = rset.getString(1);
                 Long p = rset.getLong(2);
-                Double pc = (Double.valueOf(p)/Double.valueOf(wpop))*100;
+                Double pc = (Double.valueOf(p) / Double.valueOf(wpop)) * 100;
                 output.add(n + ": " + p + " speakers, " + pc + "% of the population.");
             }
             return output;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get language reports");
             return output;
@@ -167,6 +150,7 @@ public class App
 
     /**
      * Print Countries from functions
+     *
      * @param countries: a list of countries extracted from the database
      */
     public void printAllCountries(ArrayList<Country> countries) {
@@ -218,4 +202,41 @@ public class App
             return null;
         }
     }
+
+    /**
+     * 2. All the countries in a continent organised by largest population to smallest..
+     */
+    public ArrayList<Country> getAllCountriesContPopDesc(String continent) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT code, name, continent, region, population, capital "
+                            + "FROM country "
+                            + "WHERE continent = '" + continent + "' "
+                            + " ORDER BY population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract country information
+            ArrayList<Country> countries = new ArrayList<Country>();
+            while (rset.next()) {
+                Country c = new Country();
+                c.code = rset.getString("country.Code");
+                c.name = rset.getString("country.Name");
+                c.continent = rset.getString("country.Continent");
+                c.region = rset.getString("country.Region");
+                c.population = rset.getInt("country.Population");
+                c.capital_id = rset.getInt("country.Capital");
+                countries.add(c);
+            }
+            return countries;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details by given continent");
+            return null;
+        }
+    }
+
+
 }
