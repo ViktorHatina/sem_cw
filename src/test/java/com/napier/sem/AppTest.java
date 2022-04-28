435a6d4dc08127ba51d96c528171f7348e063229
Eilidh713 <40539344@live.napier.ac.uk>
2022-04-28 20:49:02 +0100

    Updated Unit Tests

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -16,4 +16,76 @@ public class AppTest {
         app = new App();
     }
 
+    @Test
+    void printReportNull() {
+        app.printReport(null);
+    }
+
+    @Test
+    void printAllCountriesEmpty() {
+        app.printReport(new ArrayList<>());
+    }
+
+    @Test
+    void printReportContainsNull() {
+        ArrayList<String> r = new ArrayList<>();
+        r.add(null);
+        app.printReport(r);
+    }
+
+    @Test
+    void NCountriesinWorldByPopDesNegative() {
+        app.NCountriesinWorldByPopDesc(-1);
+    }
+
+    @Test
+    void NCountriesinContByPopDesNegative() {
+        app.NCountriesinContByPopDesc(-1);
+    }
+
+    @Test
+    void NCountriesinRegionByPopDesNegative() {
+        app.NCountriesinRegionByPopDesc(-1);
+    }
+
+    @Test
+    void NCitiesinWorldByPopDesc() {
+        app.NCitiesinWorldByPopDesc(-1);
+    }
+
+    @Test
+    void NCitiesinContByPopDesc() {
+        app.NCitiesinContByPopDesc(-1);
+    }
+
+    @Test
+    void NCitiesinRegionByPopDesc() {
+        app.NCitiesinRegionByPopDesc(-1);
+    }
+
+    @Test
+    void NCitiesinCountryByPopDesc() {
+        app.NCitiesinCountryByPopDesc(-1);
+    }
+
+    @Test
+    void NCitiesinDistrictByPopDesc() {
+        app.NCitiesinDistrictByPopDesc(-1);
+    }
+
+    @Test
+    void NCapsinWorldByPopDesc() {
+        app.NCapsinWorldByPopDesc(-1);
+    }
+
+    @Test
+    void NCapsinContByPopDesc() {
+        app.NCapsinContByPopDesc(-1);
+    }
+
+    @Test
+    void NCapsinRegionByPopDesc() {
+        app.NCapsinRegionByPopDesc(-1);
+    }
+
 }
